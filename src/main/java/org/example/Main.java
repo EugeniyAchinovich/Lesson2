742df4487d12f7a266caf5144594822c80362cbf
Eugene Achinovich <e.achinovitch@yandex.ru>
2023-08-28 21:30:17 +0300

    Задания 1, 2, 3, 4, 5, * в одном main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,80 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ЗАДАНИЕ 1");
+        int a, b, c;
+
+        System.out.print("b = ");
+        b = scanner.nextInt();
+        System.out.print("c = ");
+        c = scanner.nextInt();
+
+        a = 4 * (b + c - 1) / 2;
+
+        System.out.print("a = ");
+        System.out.println(a);
+
+        // Задача 2
+        System.out.println("ЗАДАНИЕ 2");
+        int n, result;
+
+        System.out.print("n = ");
+        n = scanner.nextInt();
+
+        result = n / 10 + n % 10;
+
+        System.out.print("result = ");
+        System.out.println(result);
+
+        // Задача 3
+        System.out.println("ЗАДАНИЕ 3");
+        System.out.print("n = ");
+        n = scanner.nextInt();
+
+        result = n / 100 + (n % 100) / 10 + (n % 100) % 10;
+
+        System.out.print("result = ");
+        System.out.println(result);
+
+        // Задача 4
+        System.out.println("ЗАДАНИЕ 4");
+        float x;
+
+        System.out.print("x = ");
+        x = scanner.nextFloat();
+
+        if (x - (int)x >= 0.5)
+            x++;
+
+        System.out.print("Округлённое число = ");
+        System.out.println((int)x);
+
+        // Задача 5
+        System.out.println("ЗАДАНИЕ 5");
+        int q, w;
+
+        q = scanner.nextInt();
+        w = scanner.nextInt();
+
+        System.out.println(q + " / " + w + " = " + q / w + ", остаток = "+ q % w);
+
+        // Задача *
+        System.out.println("ЗАДАНИЕ *");
+        System.out.print("a = ");
+        a = scanner.nextInt();
+        System.out.print("b = ");
+        b = scanner.nextInt();
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a + ", b = " + b);
     }
 }
